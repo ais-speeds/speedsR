2266f8bf2045fa0becbbfaf7c4a8b3dcb4a7900e
LTU Sports Data Analytics <ais.speeds@gmail.com>
2023-11-29 15:25:41 +1000

    Documentation script for the austres_df dataset.

---
+++ R/austres_df.R
@@ -0,0 +1,12 @@
+#' Quarterly Time Series of the Number of Australian Residents.
+#'
+#' Numbers (in millions) of Australian residents measured quarterly from March 1971 to March 1994. The object is of class "ts".
+#' 
+#' @format A tibble with 89 rows and 3 variables:
+#' \describe{
+#'   \item{time}{dbl Year and its quarter.}
+#'   \item{value}{dbl Australian population value in thousands.} 
+#'   \item{res_mil}{dbl Australian population value in millions.}
+#' }
+#' @source \url{https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/00Index.html}
+"austres_df"
